4029104d7e18f41ce26fa0b4cd3a696824f8bb6c
darrdv <darr.dv@gmail.com>
2022-07-09 00:30:54 -0400

    Created links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
   return (
       <header>
-          <h1>Threads</h1>
+          <Link to="/"><h1>Threads</h1></Link>
+          <Link to="/wishlist">Wish List</Link>
+          <Link to="/cart"><i class="fa-solid fa-cart-shopping"></i></Link>
       </header>
   )
 }
