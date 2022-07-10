ba494d17210cbc7f10ca9336a1ac7c43fd48a48b
darrdv <darr.dv@gmail.com>
2022-07-09 23:46:15 -0400

    Added state to context

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Context = React.createContext();
 
 const ContextProvider = (props) => {
-  return(
-    <Context.Provider value="">
+
+  const [allProducts, setAllProducts] = useState([]);
+  
+  return(    
+    //<Context.Provider value="{{allProducts:allProducts}}">  Use Object shorthand
+    <Context.Provider value="{{allProducts}}">
       {props.children}
     </Context.Provider>
   )
